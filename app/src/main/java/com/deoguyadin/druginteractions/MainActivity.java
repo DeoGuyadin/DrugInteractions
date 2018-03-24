7a0d21c5fba8f955ebda78e47230a5fa0e4e3616
Deo Guyadin <DeoGuyadin1992@Gmail.com>
2018-03-24 17:51:17 -0400

    revision 1

--- app/src/main/java/com/deoguyadin/druginteractions/MainActivity.java
+++ app/src/main/java/com/deoguyadin/druginteractions/MainActivity.java
@@ -3,8 +3,11 @@ package com.deoguyadin.druginteractions;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created By: Deo Guyadin
@@ -52,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView disclaimerView = (TextView) findViewById(R.id.disclaimer);
+        disclaimerView.setMovementMethod(new ScrollingMovementMethod());
+        disclaimerView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void goToSingleDrugActivity(View view) {
